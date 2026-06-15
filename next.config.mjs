8f0b88b37140880fd21b6a61f9dcb08cc9d0cbce
useing123 <useing@mail.ru>
2026-06-15 16:27:10 +0500

    fix: remove absolute path from next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: '/home/useing123/Desktop/job_application/solana-day-frontend',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
